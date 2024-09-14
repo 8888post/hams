5dca276d3b9c15fa091a0a32e3f745d7f03aad38
8888post <8888post@gmail.com>
2024-09-14 11:27:55 +0330

    Update ham_key_group.sh

--- ham_key_group.sh
+++ ham_key_group.sh
@@ -223,9 +223,9 @@ main() {
 		for game_choice in {1..11}; do
 			if [ $game_choice == 8 ];
    			then
-				$max=8
+				$max=8;
 			else
-				$max=4
+				$max=4;
 			fi
 			
 			for ( i=1; i <= $max; ++i )
